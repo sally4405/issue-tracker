0fb2c2d68acdeb3ad5fa50adb5bbe74557025db4
Lee SeEun <sally4405@naver.com>
2022-06-13 12:20:55 +0900

    Feat: 프로젝트 초기 설정
    - 라이브러리 추가 (swiftlint, alamofire, snapkit, RxSwift)
    - 주석 제거
    - swiftlint (white_space) 제거
    - 스토리보드 제거
    
    sally4405/issue-tracker/#1

--- GitTuul/GitTuul/ViewController.swift
+++ GitTuul/GitTuul/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  GitTuul
-//
-//  Created by Se Eun Lee on 2022/06/13.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
