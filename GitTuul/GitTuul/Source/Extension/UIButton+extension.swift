bec9ff1ecb408e090cc4553a569996c7e0f0b00c
sungju-kim <wnwn0719@gmail.com>
2022-06-13 20:02:16 +0900

    Feat: LoginViewController 구현
    - CustomView( IdView ) 구현
    - Custom UIButton+extension 구현
    
    sally4405/issue-tracker/#2

---
+++ GitTuul/GitTuul/Source/Extension/UIButton+extension.swift
@@ -0,0 +1,37 @@
+import UIKit.UIButton
+import UIKit.UIColor
+
+extension UIButton {
+
+    static let plainAttributedButton: (String) -> UIButton = { title in
+        var attribute = AttributedString.init(title)
+        attribute.font = .SFProDisplay.semiBold(16)
+
+        var plain = UIButton.Configuration.plain()
+        plain.attributedTitle = attribute
+        plain.baseForegroundColor = .Colors.primary1
+
+        let button = UIButton(configuration: plain)
+        
+        return button
+    }
+
+    static let filledAttributedButton: (String, UIImage?) -> UIButton = { title, image in
+        var attribute = AttributedString.init(title)
+        attribute.font = .SFProDisplay.semiBold(17)
+
+        var filled = UIButton.Configuration.filled()
+        filled.attributedTitle = attribute
+        filled.image = image
+        filled.imagePadding = 8
+        filled.contentInsets = NSDirectionalEdgeInsets(top: 17, leading: 0, bottom: 17, trailing: 0)
+        filled.baseBackgroundColor = .GreyScale.black
+
+        let button = UIButton(configuration: filled)
+        button.layer.cornerRadius = 20
+        button.clipsToBounds = true
+
+        return button
+    }
+
+}
