7a0b30ad8aa839cac2a2a8cccd0baed5f94cae91
sungju-kim <wnwn0719@gmail.com>
2022-06-13 17:36:28 +0900

    Feat: UIColor+extension 추가
    - UIColor.Custom
    - UIColor.GreyScale
    - UIColor.Colors
    
    sally4405/issue-tracker/#1

---
+++ GitTuul/GitTuul/Source/Extension/UIColor+Extension.swift
@@ -0,0 +1,60 @@
+import UIKit.UIColor
+
+extension UIColor {
+
+    enum Custom {
+
+        static let backgrounds1 = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        static let backgrounds2 = UIColor(red: 0.95, green: 0.95, blue: 0.97, alpha: 1.0)
+        static let backgrounds3 = UIColor(red: 0, green: 0, blue: 0, alpha: 0.02)
+
+        static let groupedBg1 = UIColor(red: 0.95, green: 0.95, blue: 0.97, alpha: 1.0)
+
+        static let label1 = UIColor(red: 0, green: 0, blue: 0, alpha: 1.0)
+        static let label2 = UIColor(red: 0.53, green: 0.53, blue: 0.55, alpha: 1.0)
+        static let label3 = UIColor(red: 0.73, green: 0.73, blue: 0.75, alpha: 1.0)
+        static let label4 = UIColor(red: 0.84, green: 0.84, blue: 0.86, alpha: 1.0)
+
+        static let fill1 = UIColor(red: 0.871, green: 0.871, blue: 0.89, alpha: 1.0)
+        static let fill2 = UIColor(red: 0.89, green: 0.89, blue: 0.91, alpha: 1.0)
+        static let fill3 = UIColor(red: 0.906, green: 0.906, blue: 0.929, alpha: 1.0)
+        static let fill4 = UIColor(red: 0.925, green: 0.925, blue: 0.949, alpha: 1.0)
+
+        static let separator1 = UIColor(red: 0.776, green: 0.776, blue: 0.784, alpha: 1.0)
+        static let separator2 = UIColor(red: 0.725, green: 0.776, blue: 0.784, alpha: 1.0)
+
+        static let disabled = UIColor(red: 0.59, green: 0.58, blue: 0.57, alpha: 1.0)
+
+    }
+
+    enum GreyScale {
+
+        static let black = UIColor(red: 0, green: 0, blue: 0, alpha: 1.0)
+        static let grey1 = UIColor(red: 0.557, green: 0.557, blue: 0.576, alpha: 1.0)
+        static let grey2 = UIColor(red: 0.682, green: 0.682, blue: 0.698, alpha: 1.0)
+        static let grey3 = UIColor(red: 0.78, green: 0.78, blue: 0.8, alpha: 1.0)
+        static let grey4 = UIColor(red: 0.82, green: 0.82, blue: 0.839, alpha: 1.0)
+        static let grey5 = UIColor(red: 0.898, green: 0.898, blue: 0.918, alpha: 1.0)
+        static let grey6 = UIColor(red: 0.949, green: 0.949, blue: 0.969, alpha: 1.0)
+        static let white = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+
+    }
+
+    enum Colors {
+
+        static let primary1 = UIColor(red: 0, green: 0.478, blue: 1.0, alpha: 1.0)
+        static let primary2 = UIColor(red: 0.782, green: 0.922, blue: 1.0, alpha: 1.0)
+        static let primary3 = UIColor(red: 0, green: 0.302, blue: 0.889, alpha: 1.0)
+        static let secondary1 = UIColor(red: 0, green: 0.145, blue: 0.904, alpha: 1.0)
+        static let secondary2 = UIColor(red: 0.799, green: 0.831, blue: 1.0, alpha: 1.0)
+        static let secondary3 = UIColor(red: 0.009, green: 0, blue: 0.437, alpha: 1.0)
+        static let error1 = UIColor(red: 1.0, green: 0.231, blue: 0.188, alpha: 1.0)
+        static let error2 = UIColor(red: 1.0, green: 0.821, blue: 0.811, alpha: 1.0)
+        static let error3 = UIColor(red: 0.766, green: 0.041, blue: 0, alpha: 1.0)
+        static let succes1 = UIColor(red: 0.204, green: 0.78, blue: 0.349, alpha: 1.0)
+        static let succes2 = UIColor(red: 0.865, green: 1.0, blue: 0.903, alpha: 1.0)
+        static let succes3 = UIColor(red: 0, green: 0.626, blue: 0.158, alpha: 1.0)
+
+    }
+
+}
